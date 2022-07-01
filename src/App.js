74da9ee0d15567e690698ab0b5fe343801a6e38e
MD Limon Farazi <abidislamabir@gmail.com>
2022-07-01 12:59:29 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import { Routes, Route, Link } from "react-router-dom";
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
